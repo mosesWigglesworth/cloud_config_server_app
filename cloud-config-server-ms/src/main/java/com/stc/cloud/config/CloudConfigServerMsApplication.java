f9b0aeb85df164871b0aa69d6a88a6e06b5d5513
mosesWigglesworth <moseswigglesworth@gmail.com>
2025-01-27 13:17:31 +0530

    Included Profiles and Logging Functionality

--- cloud-config-server-ms/src/main/java/com/stc/cloud/config/CloudConfigServerMsApplication.java
+++ cloud-config-server-ms/src/main/java/com/stc/cloud/config/CloudConfigServerMsApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author wigglesworth.m
  */
 @SpringBootApplication
 @EnableConfigServer
+@Slf4j
 public class CloudConfigServerMsApplication {
 
 	public static void main(String[] args) {
